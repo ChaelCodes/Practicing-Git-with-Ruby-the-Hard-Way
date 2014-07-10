5c0eaf68193b7ec7465071eec5cd3e941c47c5c5
RachaelsTinkerBox <rachaellw5.1@gmail.com>
2014-07-10 10:51:14 -0400

    ex9 - PARAGRAPH and whitespace
    PARAGRAPH is used to write unlimited text
    \n is a new line
    
    No Extra Credit.

---
+++ ex9.rb
@@ -0,0 +1,17 @@
+#http://ruby.learncodethehardway.org/book/ex9.html
+#NEW STUFF!!
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are some of the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+With the PARAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+This seems like a tool designed to facilitate
+hard-coding. I do not approve.
+PARAGRAPH
