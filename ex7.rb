7e3eb2c318a9829c7d23e552f676a08bdba3eb16
RachaelsTinkerBox <rachaellw5.1@gmail.com>
2014-07-10 10:34:53 -0400

    ex7 - prints vs puts
    Practicing with strings.
    I cheated on the cheeseburger one. I'd rather find out what the
    each_char method is than manually type that many var = char statements.
    #lazyprogrammerftw
    I'm not going to be doing the extra credit on these next couple. They're
    all comment on each line, do stuff on paper, and I'd rather hit some new
    stuff.

---
+++ ex7.rb
@@ -0,0 +1,26 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 10 # what'd that do?
+
+endN = []
+
+# No. I'm not playing that game.
+"CheeseBurger".each_char{|letter| endN << letter}
+
+# print instead of puts - what's the diff? puts ends in \n, print does not 
+prints endN[0] + endN[1] + endN[2] + endN[3] + endN[4] + endN[5]
+prints endN[6] + endN[7] + endN[8] + endN[9] + endN[10] + endN[11]
+
+#polite use of the terminal - leaving whitespace
+puts
+
+#So yeah, I'm not going to be doing this.
+# Extra Credit
+# For these next few exercises, you will have the exact same extra credit.
+
+# Go back through and write a comment on what each line does.
+# Read each one backwards or out loud to find your errors.
+# From now on, when you make mistakes write down on a piece of paper what kind of mistake you made.
+# When you go to the next exercise, look at the last mistakes you made and try not to make them in this new one.
+# Remember that everyone makes mistakes. Programmers are like magicians who like everyone to think they are perfect and never wrong, but it's all an act. They make mistakes all the time.
