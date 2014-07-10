a8914a232b675419883064e6ffbfff9ea315d8d6
RachaelsTinkerBox <rachaellw5.1@gmail.com>
2014-07-10 11:03:59 -0400

    ex11 - gets.chomp
    The extra credit involves finding the documentation, and I want to get
    to bigger stuff NOW. So I'll come back for these.

---
+++ ex11.rb
@@ -0,0 +1,16 @@
+#http://ruby.learncodethehardway.org/book/ex11.html
+#Time for a Physical
+
+print "How old are you?"
+age = gets.chomp()
+print "How tall are you?"
+height = gets.chomp()
+print "How much do you weigh?"
+weight = gets.chomp()
+
+puts "So, you're #{age}, about #{height} tall, and weigh #{weight}?"
+
+#EXTRA Credit - I'm sure I'll do these with validation...
+#Go online and find out what Rubys gets and chomp methods do.
+#Can you find other ways to use gets.chomp? Try some of the samples you find.
+#Write another "form" like this to ask some other questions.
