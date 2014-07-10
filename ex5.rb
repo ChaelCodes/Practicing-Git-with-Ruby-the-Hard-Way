b2bd3afdbe7027c98e5a0809edd73f61451cd323
RachaelsTinkerBox <rachaellw5.1@gmail.com>
2014-07-10 09:20:08 -0400

    ex5 - bad comments and height converted to inches
    I tried to get fancy with the height, and then the final line where you
    add the height, weight, and age failed, because 5'8" is not a number.
    Also, I don't know how to do multi-line comments yet.

--- ex5.rb
+++ ex5.rb
@@ -1,11 +1,11 @@
-/*
-	The below uses format strings to create strings using variables
-	%s is for string
-	%d is for digit
-*/
+	#http://ruby.learncodethehardway.org/book/ex5.html
+	#The below uses format strings to create strings using variables
+	#%s is for string
+	#%d is for digit
+
 my_name = 'Rachael Wright'
 my_age = 23
-my_height = "5'8\""
+my_height = 68 #inches
 my_weight = 140 #lbs
  #below are all descriptions of color
 my_eyes = 'Blue'
@@ -13,9 +13,9 @@ my_teeth = 'White'
 my_hair = 'Chestnut'
 
 puts "Let's talk about %s." % my_name
-puts "She's %s." % my_height
-puts "She weighs %d pounds."
-puts "Woof. Damn girl."
+puts "She's %d inches tall." % my_height
+puts "She weighs %d pounds." % my_weight
+puts "Woof. Damn girl. You're putting that on the internet?"
 puts "She's got %s eyes and %s hair." % [my_eyes,my_hair]
 puts "We're not going to talk about her teeth. At least they're %s." % my_teeth
 
